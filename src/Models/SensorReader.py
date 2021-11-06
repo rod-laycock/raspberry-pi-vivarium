773ca0a180b88434ebc42061996f9efc4d0678f3
Rod Laycock <rod.laycock@gmail.com>
2021-11-06 10:48:25 +0000

    Amended the SensorReader so it pulls the right mode

--- src/Models/SensorReader.py
+++ src/Models/SensorReader.py
@@ -1,9 +1,8 @@
-#import Adafruit_DHT
-import time
+from Adafruit_DHT import Adafruit_DHT
 
 class SensorReader():
   DHT_SENSOR = Adafruit_DHT.DHT11
-  MODE = None
+  MODE = ""
 
   def __init__(self, mode):
     self.MODE = mode
@@ -11,15 +10,15 @@ class SensorReader():
   def Read_Values(self, sensor):
     # Production code
     if sensor.sensorType == "DHT11":
-      DHT_SENSOR = Adafruit_DHT.DHT11
+      self.DHT_SENSOR = Adafruit_DHT.DHT11
     elif sensor.sensorType == "DHT22":
-      DHT_SENSOR = Adafruit_DHT.DHT22
+      self.DHT_SENSOR = Adafruit_DHT.DHT22
     
-    if MODE == "Dev":
+    if self.MODE == "Dev":
       humidity = sensor.pin * 20
       temperature = sensor.port * 20
     else:
-      humidity, temperature = Adafruit_DHT.read_retry(DHT_SENSOR, sensor.Get_Pin())
+      humidity, temperature = Adafruit_DHT.read_retry(self.DHT_SENSOR, sensor.Get_Pin())
 
     return humidity, temperature
 
