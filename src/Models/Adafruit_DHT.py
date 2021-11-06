3f85aa32c746aa8026d2e26c4f98b0a339bd6e5b
Rod Laycock <rod.laycock@gmail.com>
2021-11-06 09:55:51 +0000

    Added a stub to allow me to run the code on laptop

---
+++ src/Models/Adafruit_DHT.py
@@ -0,0 +1,14 @@
+# This is a stub so that it runs on my laptop
+from datetime import datetime
+
+class Adafruit_DHT():
+  DHT11 = 1
+  DHT22 = 2
+
+  def read_retry(self, DHT_SENSOR, pin):
+    now = datetime.now()
+    
+    humidity = now.min
+    temperature = now.second
+    
+    return humidity, temperature
