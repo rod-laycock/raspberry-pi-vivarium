58d3affacb2f603c139626b387aa1c1426fd411b
Rod Laycock <rod.laycock@gmail.com>
2021-12-21 08:39:02 +0000

    Added ability to obtain sensors by Id / list of all sensors

--- src/Webservice/Models/Sensor.py
+++ src/Webservice/Models/Sensor.py
@@ -1,16 +1,17 @@
+from json import JSONEncoder
+
 # Sensor
 #   Name - so we can see it on screen
 #   Port - Which port on the back of the system is this connected too?
 #   Pin - So the sensor reader can request values on the pi
-#   Comment - useless crap
+#   Comment - Human readable comment on this.
 #   MinTemp - minimum temperature this sensor is allowed to get too
 #   MaxTemp - maximum temperature this sensor is allowed to get too 
-#   TempUnit - Should this be in here?
-
+#   MinHumidity - minimum humidity this sensor is allowed to get too
+#   MaxHumidity - maximum humidity this sensor is allowed to get too
 
 class Sensor():
-
-    def __init__(self, name, port, pin, sensorType, comment, minTemp, maxTemp, tempUnit):
+    def __init__(self, name, port, pin, sensorType, comment, minTemp, maxTemp, minHumidity, maxHumidity):
         self.name = name
         self.port = port
         self.pin = pin
@@ -18,6 +19,10 @@ class Sensor():
         self.comment = comment
         self.minTemp = minTemp
         self.maxTemp = maxTemp
-        self.tempUnit = tempUnit  #  Do we need this?
-    
-  
+        self.minHumidity = minHumidity
+        self.maxHumidity = maxHumidity
+
+# Json Encoder 
+class SensorEncoder(JSONEncoder):
+        def default(self, s):
+            return s.__dict__
--- src/Webservice/api.py
+++ src/Webservice/api.py
@@ -1,38 +1,105 @@
-from flask import Flask
-from flask_restful import Resource, Api
 import json
+import time
+
+from flask import Flask
+from flask_restful import Api
 from distutils.util import strtobool
+from Models.Sensor import Sensor, SensorEncoder
+from Models.SensorReader import SensorReader
 
 app = Flask(__name__)
 api = Api(app)
 
-class HelloWorld(Resource):
-    def get(self):
-        return {'hello': 'world'}
+# Variable to hold the sensors collection
+sensors = {}
 
 
-api.add_resource(HelloWorld, '/')
 
+#
+# Sensor
+#
+@app.route('/sensors', methods=['GET'])
+def get_sensors():
+    return json.dumps(sensors, indent=2, cls=SensorEncoder)
+
+# TODO: Return data 
+@app.route('/sensors/<int:port>', methods=['GET'])
+def get_sensor_by_id(port):
+    sensor = sensors.get(str(port))
+    return json.dumps(sensor, indent=2, cls=SensorEncoder)
+
+#
+# Config - Get the current config values, factory default values and any allowable values.
+#
+def read_config(config):
+    with open('Config/' + config + '.json', 'r') as configFile:
+        configData = configFile.read()
+    
+    config = json.loads(configData)
+    
+    return config
+
+@app.route('/config/current', methods=['GET'])
+def get_current_config():
+    return read_config('config')
+
+@app.route('/config/factory', methods=['GET'])
+def get_factory_config():
+    return read_config('factory')
+
+@app.route('/config/values', methods=['GET'])
+def get_config_values():
+    return read_config('defaults')
+
+#
+# Main entry point.
+#
 if __name__ == '__main__':
     # Defaults
     server_host = "127.0.0.1"
     server_port = 5000
     server_debug = False
 
-    # read file
     with open('Config/config.json', 'r') as configFile:
         configData = configFile.read()
-    # parse file
     config = json.loads(configData)
 
-    if ('Server' in config):
-        if ('Host' in config['Server']):
-            server_host = config['Server']['Host']
+    if (config != None):
+        if ('Server' in config):
+            if ('Host' in config['Server']):
+                server_host = config['Server']['Host']
 
-        if ('Port' in config['Server']):
-            server_port = config['Server']['Port']
+            if ('Port' in config['Server']):
+                server_port = config['Server']['Port']
+            
+            if ('Debug' in config['Server']):
+                server_debug = bool(strtobool(config['Server']['Debug']))
         
-        if ('Debug' in config['Server']):
-            server_debug = bool(strtobool(config['Server']['Debug']))
+        pollFrequency = config['PollFrequency']
+        mode = config['Mode']
+        tempUnit = config['TempUnit']
+        DateTime = config['DateTime']
+        DateTime_TimeZone = DateTime['TimeZone']
+        DateTime_Format = DateTime['Format']
 
-    app.run(debug=bool(server_debug), host=server_host, port=server_port)
+        if ('Sensors' in config):
+            for sensor in config['Sensors']:
+                port = sensor['Port']
+                sensorObj = Sensor(sensor['Name'], port, sensor['Pin'], sensor['SensorType'], sensor['Comment'], sensor['MinTemp'], sensor['MaxTemp'], sensor['MinHumidity'], sensor['MaxHumidity'])
+                sensors[str(port)] = sensorObj
+        
+        app.run(debug=bool(server_debug), host = server_host, port = server_port)
+
+        while True:
+            for sensorPort in sensors:
+                if sensorPort != None:
+                    sensor = sensors[sensorPort]
+                    dateTime, humidity, temperature = SensorReader.Read_Values(sensor)
+                    localtime = time.strftime(DateTime_Format, dateTime)
+
+                    if humidity is not None and temperature is not None:
+                        print("{1}: {0} Temp={2:0.1f}*C Hunmidity={3:0.1f}%".format(sensor.name, localtime, temperature, humidity))
+                    else:
+                        print("Failed to retreive data from sensor")
+                time.sleep(pollFrequency)
+        
