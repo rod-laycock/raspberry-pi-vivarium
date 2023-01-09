65ce7d3573faa142892541bb7363791f2c203aa8
Rod Laycock <rod.laycock@gmail.com>
2023-01-09 22:51:25 +0000

    Added sample template

--- src/ui/__init__.py
+++ src/ui/__init__.py
@@ -1,5 +1,5 @@
 # /flask_2/__init__.py
-from flask import Flask, Response
+from flask import Flask, render_template, Response
 
 app = Flask(__name__)
 app.debug = True
@@ -7,5 +7,5 @@ app.debug = True
 @app.route("/", methods=["GET"], )
 def get_home():
 
-  #return render_template('index.html')
-  return Response("Vivarium Monitoring Website Application up and running.", status=200, mimetype="text/html")
+  return render_template('index.html')
+  #return Response("Vivarium Monitoring Website Application up and running.", status=200, mimetype="text/html")
